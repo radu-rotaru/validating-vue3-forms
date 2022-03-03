f678377fc5eeeed6f6350c7feb0c577280c4d019
unknown <rotrast@gmail.com>
2022-03-03 15:32:45 +0200

    Completed L2

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,35 +1,38 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <BaseInput
-      label="Email"
-      type="email"
-      v-model="email"
-      :error="emailError"
-    />
+    <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
 
-    <BaseInput
-      label="Password"
-      type="password"
-    />
+    <BaseInput label="Password" type="password" />
 
-    <BaseButton
-      type="submit"
-      class="-fill-gradient"
-    >
-    Submit
-    </BaseButton>
+    <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
   </form>
 </template>
 
 <script>
+import { useField } from 'vee-validate'
 export default {
   setup () {
     function onSubmit () {
       alert('Submitted')
     }
 
+    const email = useField('email', function (value) {
+      if (!value) {
+        return 'This field is required'
+      }
+      const regex =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if (!regex.test(String(value).toLowerCase())) {
+        return 'Please enter a valid email adress'
+      }
+
+      return true
+    })
+
     return {
-      onSubmit
+      onSubmit,
+      email: email.value,
+      emailError: email.errorMessage
     }
   }
 }
